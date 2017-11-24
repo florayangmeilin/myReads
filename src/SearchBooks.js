9162a7b9503e00ea49248c79ad055cd6f9e7caed
flyingshoes <flyingshoes@163.com>
2017-11-24 17:20:08 +0800

    feat:Complete search function

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,23 +1,47 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import escapeRegExp from 'escape-string-regexp'
 import Book from './Book'
 
 class SearchBooks extends Component {
+    state = {
+        query: ''
+    }
+
+    updateQuery = (query) => {
+        this.setState({ query: query.trim() })
+    }
+
+
     render() {
         const { books } = this.props
+        const { query } = this.state
+
+        let showingBooks
+        if (query) {
+            const match = new RegExp(escapeRegExp(query), 'i')
+            showingBooks = books.filter((book) => match.test(book.title) || match.test(book.authors))
+        } else {
+            showingBooks = books
+        }
 
         return (
             <div className="search-books">
                 <div className="search-books-bar">
                     <Link className='close-search' to='/'>Close</Link>
                     <div className="search-books-input-wrapper">
-                        <input type="text" placeholder="Search by title or author" />
+                        <input
+                            type="text"
+                            placeholder="Search by title or author"
+                            value={query}
+                            onChange={(event) => this.updateQuery(event.target.value)}
+                        />
                     </div>
                 </div>
                 <div className="search-books-results">
                     <ol className="books-grid">
-                        {books.map((book) => (
-                            <li>
+                        {showingBooks.map((book) => (
+                            <li key={book.id}>
                                 <Book book={book} />
                             </li>
                         ))}
